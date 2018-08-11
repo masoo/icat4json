07aedab12542e9a9c3414fa23fba76579a13366d
FUNABARA Masao <masao@masoo.jp>
2018-08-11 22:33:34 +0900

    add Icat4json Class

--- icat4json.gemspec
+++ icat4json.gemspec
@@ -9,9 +9,9 @@ Gem::Specification.new do |spec|
   spec.authors       = ["FUNABARA Masao"]
   spec.email         = ["masao@masoo.jp"]
 
-  spec.summary       = %q{TODO: Write a short summary, because RubyGems requires one.}
-  spec.description   = %q{TODO: Write a longer description or delete this line.}
-  spec.homepage      = "TODO: Put your gem's website or public repo URL here."
+  spec.summary       = "IPA's \"icat for JSON\" ruby package"
+  spec.description   = "IPA's \"icat for JSON\" ruby package"
+  spec.homepage      = "https://github.com/masoo/icat4json"
   spec.license       = "MIT"
 
   # Prevent pushing this gem to RubyGems.org. To allow pushes either set the 'allowed_push_host'
--- lib/icat4json.rb
+++ lib/icat4json.rb
@@ -1,5 +1,30 @@
 require "icat4json/version"
+require "net/http"
+require "uri"
+require "json"
 
-module Icat4json
-  # Your code goes here...
+module ICat4JSON
+  class ICat4JSON
+    attr_accessor :json, :icat
+    ICAT4JSON_URL = "http://isec-myjvn-feed1.ipa.go.jp/IPARssReader.php?1456207434&tool=icatw"
+
+    def initialize
+      uri = URI.parse(ICAT4JSON_URL)
+      http = Net::HTTP.get(uri)
+      @json = JSON.parse(http)
+      struct_icat = Struct.new(:itemdata, :docTitle, :docTitleFix, :docLink, :docDate)
+      struct_item = Struct.new(:item_title, :item_link, :item_date, :item_identifier)
+      @icat = struct_icat.new
+      @json.each {|k, v| icat[k] = v unless k == "itemdata" }
+      @icat[:itemdata] = []
+      @json["itemdata"].each do |itemdata|
+        item = struct_item.new
+        itemdata.each do |k,v|
+          item[k] = v
+        end
+        @icat[:itemdata].push item
+      end
+      true
+    end
+  end
 end
