7c736e1420eb906df7b756df3671a6d5d4de53d8
FUNABARA Masao <masao@masoo.jp>
2025-09-23 04:17:08 +0000

    Refactor to make it simpler and easier to understand.

--- lib/icat4json.rb
+++ lib/icat4json.rb
@@ -6,25 +6,32 @@ require "json"
 module ICat4JSON
   class ICat4JSON
     attr_accessor :json, :icat
-    ICAT4JSON_URL = "http://isec-myjvn-feed1.ipa.go.jp/IPARssReader.php?1456207434&tool=icatw"
+
+    ICATData = Struct.new(:itemdata, :docTitle, :docTitleFix, :docLink, :docDate)
+    ItemData = Struct.new(:item_title, :item_link, :item_date, :item_identifier)
 
     def initialize
-      uri = URI.parse(ICAT4JSON_URL)
+      uri = URI.parse(icat4json_url)
       http = Net::HTTP.get(uri)
       @json = JSON.parse(http)
-      struct_icat = Struct.new(:itemdata, :docTitle, :docTitleFix, :docLink, :docDate)
-      struct_item = Struct.new(:item_title, :item_link, :item_date, :item_identifier)
-      @icat = struct_icat.new
+      @icat = ICATData.new
       @json.each {|k, v| icat[k] = v unless k == "itemdata" }
       @icat[:itemdata] = []
       @json["itemdata"].each do |itemdata|
-        item = struct_item.new
+        item = ItemData.new
         itemdata.each do |k,v|
           item[k] = v
         end
         @icat[:itemdata].push item
       end
       true
+    end
+
+    private
+
+    # analize https://www.ipa.go.jp/security/announce/irss/icatw2.js
+    def icat4json_url
+      "http://isec-myjvn-feed1.ipa.go.jp/IPARssReader.php?#{Time.now.to_i}&tool=icatw"
     end
   end
 end
